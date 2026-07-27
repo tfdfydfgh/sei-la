161c90d94692d422e48a61575b1c97d7bf2dae50
tfdfydfgh <davi14012011@gmail.com>
2026-07-27 19:59:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    botao.addEventListener("click", botaoClicado);
+    function botaoClicado() {
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+        } else {
+            texto.textContent--;
+            curtiu = false
+        }
+    }
+})
+
+const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+btnTemaEscuro.addEventListener("click", mudarTema);
+function mudarTema() {
+    const corpoPagina = document.body;
+ if (corpoPagina.classList.contains("tema-escuro")) {
+    corpoPagina.classList.remove("tema-escuro");
+} else{
+    corpoPagina.classList.add("tema-escuro");
+}
+}
